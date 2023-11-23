9ba4b5e8694d2276d450efab88bfce60d2084d7e
Ruann Fagundes <ruanndev@gmail.com>
2023-11-23 02:25:13 -0300

    Added Desktop Responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -42,24 +42,41 @@ function App() {
   };
 
   return (
-    <main className="text-gray-200 font-light p-5 flex flex-col gap-5 md:gap-10">
-      <div className="flex flex-row justify-between">
-        <img
-          onClick={changeColor}
-          className={`${gradient} md:w-32`}
-          src={brand.src}
-          width={brand.width}
-          alt="RuannJS Logo"
-        />
-        <div className="flex flex-row gap-10 md:gap-20 items-center mr-2">
-          {socials.map((social) => {
-            if (social.name === "gmail") {
+    <main className="text-gray-200 font-light p-5 flex flex-col 2xl:flex-row  2xl:pl-20">
+      <div className="2xl:px-96 2xl:py-10">
+        <div className="flex flex-row justify-between">
+          <img
+            onClick={changeColor}
+            className={`${gradient} md:w-32`}
+            src={brand.src}
+            width={brand.width}
+            alt="RuannJS Logo"
+          />
+          <div className="flex flex-row gap-10 md:gap-20 items-center mr-2 2xl:ml-16">
+            {socials.map((social) => {
+              if (social.name === "gmail") {
+                return (
+                  <a
+                    key={social.name}
+                    className=""
+                    target="_blank"
+                    href={`mailto:${social.link}`}
+                  >
+                    <img
+                      className="md:w-16"
+                      width={30}
+                      src={social.icon}
+                      alt="social"
+                    />
+                  </a>
+                );
+              }
               return (
                 <a
                   key={social.name}
                   className=""
                   target="_blank"
-                  href={`mailto:${social.link}`}
+                  href={social.link}
                 >
                   <img
                     className="md:w-16"
@@ -69,103 +86,89 @@ function App() {
                   />
                 </a>
               );
-            }
-            return (
-              <a
-                key={social.name}
-                className=""
-                target="_blank"
-                href={social.link}
-              >
-                <img
-                  className="md:w-16"
-                  width={30}
-                  src={social.icon}
-                  alt="social"
-                />
-              </a>
-            );
-          })}
+            })}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-10">
-        <span className="text-gray-200 font-black text-5xl md:text-7xl">
-          Welcome.
-        </span>
-        <div className="flex flex-row items-start gap-3">
-          <span className={`${gradient} pb-56 animate-pulse`}></span>
-          <div className="flex flex-col gap-5 text-sm text-gray-200 font-light md:pe-96">
-            {isProject ? (
-              <div className="flex flex-col text-base gap-5 text-slate-200 sm:text-lg md:text-xl">
-                <span>{currentProject?.description}</span>
-                {currentProject?.type === "front" ? (
-                  <>
-                    <a
-                      target="_blank"
-                      className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max hover:text-xl cursor-pointer font-bold sm:text-2xl md:text-3xl`}
-                      href={currentProject.link}
-                    >
-                      View Site
-                    </a>
-                    <div className="flex flex-row gap-5 md:gap-10">
-                      {currentProject.stack.map((stack) => {
-                        return (
-                          <span
-                            key={stack}
-                            className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max text-xs font-bold sm:text-base md:text-lg`}
-                          >
-                            {stack}
-                          </span>
-                        );
-                      })}
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <a
-                      target="_blank"
-                      className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max hover:text-xl cursor-pointer font-bold sm:text-2xl md:text-3xl`}
-                      href={currentProject?.link}
-                    >
-                      View Repository
-                    </a>
-                    <div className="flex flex-row gap-5 flex-wrap md:gap-10 md:flex-nowrap">
-                      {currentProject?.stack.map((stack) => {
-                        return (
-                          <span
-                            key={stack}
-                            className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max text-xs font-bold sm:text-base md:text-lg`}
-                          >
-                            {stack}
-                          </span>
-                        );
-                      })}
-                    </div>
-                  </>
-                )}
-              </div>
-            ) : (
-              <div className="flex flex-col gap-5 text-sm text-gray-200 font-light sm:text-lg md:text-xl">
-                <span>
-                  My name is Ruann Fagundes. I'm a full-stack developer based in
-                  Brazil.
-                </span>
-                <span>
-                  I'm eagerly looking for opportunities in the tech industry as
-                  a trainee or junior developer to kickstart my career.
-                </span>
-                <span>
-                  While I may not have formal work experience, I've dedicated
-                  three years to self-study and practical projects, building a
-                  strong foundation in front and backend development.
-                </span>
-              </div>
-            )}
+        <div className="flex flex-col gap-10">
+          <span className="text-gray-200 font-black text-5xl md:text-7xl 2xl:mt-10">
+            Welcome.
+          </span>
+          <div className="flex flex-row items-start gap-3">
+            <span
+              className={`${gradient} pb-56 animate-pulse 2xl:pb-72`}
+            ></span>
+            <div className="flex flex-col gap-5 text-sm text-gray-200 font-light">
+              {isProject ? (
+                <div className="flex flex-col text-base gap-5 text-slate-200 sm:text-lg md:text-xl">
+                  <span className="">{currentProject?.description}</span>
+                  {currentProject?.type === "front" ? (
+                    <>
+                      <a
+                        target="_blank"
+                        className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max hover:text-xl cursor-pointer font-bold sm:text-2xl md:text-3xl`}
+                        href={currentProject.link}
+                      >
+                        View Site
+                      </a>
+                      <div className="flex flex-row gap-5 md:gap-10">
+                        {currentProject.stack.map((stack) => {
+                          return (
+                            <span
+                              key={stack}
+                              className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max text-xs font-bold sm:text-base md:text-lg`}
+                            >
+                              {stack}
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <a
+                        target="_blank"
+                        className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max hover:text-xl cursor-pointer font-bold sm:text-2xl md:text-3xl`}
+                        href={currentProject?.link}
+                      >
+                        View Repository
+                      </a>
+                      <div className="flex flex-row gap-5 flex-wrap md:gap-10 md:flex-nowrap 2xl:flex-wrap">
+                        {currentProject?.stack.map((stack) => {
+                          return (
+                            <span
+                              key={stack}
+                              className={`${gradient} bg-[length:100%_1px] bg-no-repeat bg-bottom max-w-max text-xs font-bold sm:text-base md:text-lg`}
+                            >
+                              {stack}
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </>
+                  )}
+                </div>
+              ) : (
+                <div className="flex flex-col gap-5 text-sm text-gray-200 font-light sm:text-lg md:text-xl">
+                  <span>
+                    My name is Ruann Fagundes. I'm a full-stack developer based
+                    in Brazil.
+                  </span>
+                  <span>
+                    I'm eagerly looking for opportunities in the tech industry
+                    as a trainee or junior developer to kickstart my career.
+                  </span>
+                  <span>
+                    While I may not have formal work experience, I've dedicated
+                    three years to self-study and practical projects, building a
+                    strong foundation in front and backend development.
+                  </span>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="">
+      <div className="2xl:mr-96 2xl:my-32">
         <span className="text-4xl font-bold text-gray-200 md:text-5xl">
           Projects
         </span>
